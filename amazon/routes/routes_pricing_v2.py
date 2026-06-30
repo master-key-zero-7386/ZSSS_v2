628a4efe335f147fcb4efc5411e35207e749cc41
ZSSS <zsss@example.com>
2026-06-30 11:56:21 +0900

    Active判定チェック1

--- amazon/routes/routes_pricing_v2.py
+++ amazon/routes/routes_pricing_v2.py
@@ -1002,6 +1002,8 @@ def update_listing_price(*, user_id: int, asin: str, country_code: str):
             pass
                
     # === -12: DB更新 ===
+    print(asin, final_price, status_value)  # チェック完了後削除
+
     conn = get_conn(listed_db)
 
     try:
