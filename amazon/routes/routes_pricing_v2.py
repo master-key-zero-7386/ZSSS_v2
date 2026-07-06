b544554e049ef4ff1a94c54daf4329c750f56138
ZSSS <zsss@example.com>
2026-07-06 20:10:34 +0900

    自分セラーID除外修正3

--- amazon/routes/routes_pricing_v2.py
+++ amazon/routes/routes_pricing_v2.py
@@ -383,7 +383,7 @@ def update_region_pricing(*, user_id: int, asin: str, country_code: str, home_pr
         SELECT account_seller_id
         FROM account_master
         WHERE user_id = %s
-        AND country_code = %s
+        AND UPPER(country_code) = UPPER(%s)
         LIMIT 1
     """, (user_id, country_code))
 
